153750bfc6d2f3f8606e51a31554faf1cc842257
Pharnuwat <se56160411@gmail.com>
2025-09-16 14:07:27 +0700

    feat: update global tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Merry Match</h1>
+      <h1 className="header-1">Merry Match</h1>
     </div>
   );
 }
