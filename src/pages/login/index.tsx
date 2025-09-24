80950cb0a7bd038845cb9d3bfa45a2a07913171f
Ponlawat <p.sangtub@gmail.com>
2025-09-24 14:43:08 +0700

    refactor: consolidate login page components into a single file

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,13 @@
-import LoginPage from './LoginPage';
+import NavBarNonUser from "@/components/NavBarNonUser";
+import Login from "@/components/login/Login";
+
+function LoginPage() {
+    return (
+        <div className="min-h-screen bg-white shadow-lg">
+            <NavBarNonUser />
+            <Login />
+        </div>
+    );
+}
 
 export default LoginPage;
