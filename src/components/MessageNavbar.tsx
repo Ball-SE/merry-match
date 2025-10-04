1c3f36ae6905bf718d63ee373fd2c0c8ff4e6ecf
Ponlawat <p.sangtub@gmail.com>
2025-10-04 14:12:03 +0700

    feat(chat): link message icon in user navbar

--- src/components/MessageNavbar.tsx
+++ src/components/MessageNavbar.tsx
@@ -1,10 +1,13 @@
 import { LuMessageCircleMore } from "react-icons/lu";
+import Link from "next/link";
 
 function MessageNavbar() {
     return (
-        <div className="w-[28px] h-[28px] sm:w-[48px] sm:h-[48px] rounded-full bg-[#F6F7FC] flex justify-center items-center">
-            <LuMessageCircleMore className="w-[20px] h-[21px]" color="#FFB1C8"/>
-        </div>
+        <Link href="/chat">
+            <div className="w-[28px] h-[28px] sm:w-[48px] sm:h-[48px] rounded-full bg-[#F6F7FC] flex justify-center items-center cursor-pointer hover:bg-[#FFB1C8] hover:bg-opacity-20 transition-colors">
+                <LuMessageCircleMore className="w-[20px] h-[21px]" color="#FFB1C8"/>
+            </div>
+        </Link>
     )
 }
 
